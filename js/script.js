eb2ceae755f2ec7c0070dfd93512ce2dc3bea710
Piotr Kiczkajło <kiczkajlo.p@gmail.com>
2021-05-05 13:21:56 +0200

    add list function

--- js/script.js
+++ js/script.js
@@ -1,28 +1,36 @@
 {
     const tasks = [{
             content: "zaprowadzić Kacpra",
-            done: true
+            done: false
         },
         {
             content: "zrobić zakupy",
-            done: false
+            done: true
         },
     ];
-
     const render = () => {
         let htmlString = "";
         for (const task of tasks) {
             htmlString += `
-        <li
+            <li 
+            ${task.done ? "class=\"list__item--done\"" : ""}" >
+            <button class="js-done">Zrobione</button>
             ${task.content}
+            <button class="js-remove">Usuń</button>
         </li>
         `
         };
         document.querySelector(".js-listTask").innerHTML = htmlString;
     };
     const init = () => {
-        render();
-    };
+        const formTask = document.querySelector(".js-formTask");
+        formTask.addEventListener("submit", (event) => {
+            event.preventDefault();
+            const newTask = document.querySelector(".js-newTask").value.trim();
+            console.log(newTask);
+        });
 
+    };
+    render();
     init();
 }
