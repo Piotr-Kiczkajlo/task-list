cc9b0e2c0ab424eaf1a2103f0f4c34a75dceb609
Piotr Kiczkajło <kiczkajlo.p@gmail.com>
2021-05-24 23:00:34 +0200

    add new  buttons and refactoring js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 {
-    const tasks = [];
+    let tasks = [];
+    let hideDoneTasks = false;
+
     const newTaskAdd = () => {
         const newTask = document.querySelector(".js-inputText");
         const newTaskText = newTask.value.trim();
@@ -7,26 +9,40 @@
             newTask.focus();
             return;
         }
-        tasks.push({
-            content: newTaskText,
-        });
+        tasks = [
+            ...tasks,
+            { content: newTaskText, }
+        ];
         newTask.value = "";
     };
-    const addCheckedButton = () => {
+    const toggleCheckTask = (index) => {
+        tasks = [
+            ...tasks.slice(0, index),
+            {...tasks[index], done: !tasks[index].done },
+            ...tasks.slice(index + 1),
+        ];
+        render();
+    };
+    const deleteTask = index => {
+        tasks = [
+            ...tasks.slice(0, index),
+            ...tasks.slice(index + 1),
+        ];
+        render();
+    };
+    const addCheckedEvent = () => {
         const checkButtons = document.querySelectorAll(".js-check");
         checkButtons.forEach((checkButton, index) => {
             checkButton.addEventListener("click", () => {
-                tasks[index].done = !tasks[index].done;
-                render();
+                toggleCheckTask(index);
             });
         });
     };
-    const addDeleteButton = () => {
+    const addDeleteEvent = () => {
         const deleteButtons = document.querySelectorAll(".js-delete");
         deleteButtons.forEach((deleteButton, index) => {
             deleteButton.addEventListener("click", () => {
-                tasks.splice(index, 1);
-                render();
+                deleteTask(index);
             });
         });
     };
@@ -35,19 +51,58 @@
         newTaskAdd();
         render();
     };
-    const render = () => {
+    const renderTasks = () => {
         let htmlString = "";
         for (const task of tasks) {
             htmlString += `
-            <li class="list__item">
-            <button class="list__button list__button--check  js-check">${task.done ? "&check;"  : "" }</button>
-                <span class="list__item--content ${task.done ? "list__item--done" : "" }">${task.content}</span>
-            <button class="list__button list__button--delete js-delete">&#128465;</button>
-            </li> `
+        <li class="list__item ${task.done && hideDoneTasks ? "list__item--hide" : ""}">
+        <button class="list__button list__button--check  js-check">${task.done ? "&check;"  : "" }</button>
+            <span class="list__item--content ${task.done ? "list__item--done" : "" }">${task.content}</span>
+        <button class="list__button list__button--delete js-delete">&#128465;</button>
+        </li> `
         }
         document.querySelector(".js-listTask").innerHTML = htmlString;
-        addCheckedButton();
-        addDeleteButton();
+    };
+    const markAllTask = () => {
+        tasks = tasks.map(task => ({
+            ...task,
+            done: true,
+        }));
+        render();
+    };
+    const toggleHideDoneTask = () => {
+        hideDoneTasks = !hideDoneTasks;
+        render();
+    };
+    const finishAllTasksEvents = () => {
+        const finishAllTasks = document.querySelector(".js-finishTasks");
+        finishAllTasks.addEventListener("click", () => {
+            markAllTask();
+        });
+    };
+    const hideFinishTasksEvents = () => {
+        const hideFinishTasks = document.querySelector(".js-hideTasks");
+        hideFinishTasks.addEventListener("click", () => {
+            toggleHideDoneTask();
+        });
+    };
+    const renderHeaderButtons = () => {
+        let manageTasksButton = "";
+        if (tasks.length > 0) {
+            manageTasksButton += `
+            <button class="manageTasksButton js-hideTasks"> ${hideDoneTasks  ? "Pokaż ukończone" : "Ukryj ukończone"}</button>
+            <button class="manageTasksButton js-finishTasks" ${tasks.every(({done}) => done) ? "disabled" : ""}>Ukończ wszystkie</button>    
+            `
+        };
+        document.querySelector(".js-headerButtons").innerHTML = manageTasksButton;
+    };
+    const render = () => {
+        renderTasks();
+        renderHeaderButtons();
+        finishAllTasksEvents();
+        hideFinishTasksEvents();
+        addCheckedEvent();
+        addDeleteEvent();
     };
     const init = () => {
         const formTask = document.querySelector(".js-formTask");
